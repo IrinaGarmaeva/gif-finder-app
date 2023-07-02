07f13368959712b008b4d828dc2e65bdcf6cb5bd
Polina Avdeeva <polinaavdeeva55@gmail.com>
2023-07-02 04:34:57 +0300

    feat: add api logic

---
+++ src/utils/Api.js
@@ -0,0 +1,43 @@
+class Api {
+    constructor({ url, apiKey }) {
+        this._url = url;
+        this._apiKey = apiKey;
+    }
+
+    searchGifs(query, limit, offset) {
+        const url = `${this._url}/search?q=${query}&api_key=${this._apiKey}&limit=${limit}&offset=${offset}`;
+
+        return fetch(url)
+            .then(this._checkResponse)
+            .catch(error => console.error(error));
+    }
+
+    getTrendingGifs(limit, offset) {
+        const url = `${this._url}/trending?api_key=${this._apiKey}&limit=${limit}&offset=${offset}`;
+
+        return fetch(url)
+            .then(this._checkResponse)
+            .catch(error => console.error(error));
+    }
+
+    getRandomGifs() {
+        const url = `${this._url}/random?api_key=${this._apiKey}`;
+
+        return fetch(url)
+            .then(this._checkResponse)
+            .catch(error => console.error(error));
+    }
+
+    _checkResponse = (response) => {
+        if (response.ok) {
+            return response.json();
+        }
+
+        return Promise.reject(`Ошибка: ${response.status}`);
+    }
+}
+
+export const api = new Api({
+    url: 'https://api.giphy.com/v1/gifs',
+    apiKey: 'D62Zd7oP6sGSpahKphi5CUJwTgIy6kgn'
+});
